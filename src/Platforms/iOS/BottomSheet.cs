1780f325d0eba9f9f9fcf49af23d2de81258930e
Paul Varache <paulvarache@Pauls-Mac-mini.local>
2023-07-10 14:10:47 +0100

    WIP

--- src/Platforms/iOS/BottomSheet.cs
+++ src/Platforms/iOS/BottomSheet.cs
@@ -6,28 +6,8 @@ public partial class BottomSheet
 {
     public BottomSheetViewController Controller { get; set; }
 
-    // Can't get the sheet max height with large and medium detents
-    // custom detents are not supported on iOS 15
-    // can't use largestUndimmedIdentifier or selected detent with custom detents on iOS 16
-    // So I guess we'll just have to calculate the sheet height ourselves then
-    // This number was found by getting the full screen height, subtracting the sheet's UIView height and the top inset
-    // This seems to be the spacing iOS leaves at the top of the screen when a sheet is fullscreen
-    // TODO: Check if this is the same number for fullscreen sheets opened on top of another sheet
-    const int SheetTopSpacing = 10;
-
     // Cache the calculated detents as iOS likes to ask for detents often
     internal readonly IDictionary<int, float> CachedDetents = new Dictionary<int, float>();
 
-    double _maximumDetentValue = -1;
-
-    partial void CalculateTallestDetent(double heightConstraint)
-    {
-        if (_maximumDetentValue == -1)
-        {
-            var window = UIApplication.SharedApplication.KeyWindow;
-            var topPadding = window?.SafeAreaInsets.Top ?? 0;
-            _maximumDetentValue = Window.Height - topPadding - SheetTopSpacing;
-        }
-        _tallestDetent = GetEnabledDetents().Select(d => d.GetHeight(this, _maximumDetentValue)).Max();
-    }
+    
 }
--- src/Platforms/iOS/Models/AnchorDetent.cs
+++ src/Platforms/iOS/Models/AnchorDetent.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using CoreGraphics;
-using UIKit;
+﻿using UIKit;
 
 namespace The49.Maui.BottomSheet;
 
@@ -9,11 +7,7 @@ public partial class AnchorDetent
     partial void UpdateHeight(BottomSheet page, double maxSheetHeight)
     {
         var pageView = (UIView)page.Handler.PlatformView;
-        pageView.Frame = new CGRect(new CGPoint(0, 0), new CGSize(page.Window.Width, maxSheetHeight));
         var targetView = (UIView)Anchor.Handler.PlatformView;
-
-        pageView.SetNeedsLayout();
-        pageView.LayoutIfNeeded();
 
         var targetOrigin = targetView.Superview.ConvertPointToView(targetView.Frame.Location, pageView);
 
