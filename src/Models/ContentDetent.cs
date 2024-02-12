e81a463ee9c0d84b2f5b7d2956954eccb87e09ca
borrmann <max.borrmann@gmx.net>
2024-02-12 14:04:20 +0000

    removed null reference when page is null (Android exception on dismiss)

--- src/Models/ContentDetent.cs
+++ src/Models/ContentDetent.cs
@@ -4,15 +4,19 @@ public partial class ContentDetent : Detent
 {
     public override double GetHeight(BottomSheet page, double maxSheetHeight)
     {
-        if (page.Content is null)
+        if (page?.Content is null)
         {
             return maxSheetHeight;
         }
-        if (page.Content is ScrollView sv)
-        {
-            var s = sv.Content.Measure(page.Window.Width - page.Padding.HorizontalThickness, maxSheetHeight);
-        }
-        var r = page.Content.Measure(page.Window.Width - page.Padding.HorizontalThickness, maxSheetHeight);
+
+        // I left this code as comment because it might be useful for future reference, but it's not used
+        //if (page.Content is ScrollView sv)
+        //{
+        //    var s = sv.Content.Measure(page.Window.Width - page.Padding.HorizontalThickness, maxSheetHeight);
+        //}
+
+        // HACK: from 'page.Window.Width' to 'page.Window?.Width ?? page.Width' 
+        var r = page.Content.Measure(page.Window?.Width ?? page.Width - page.Padding.HorizontalThickness, maxSheetHeight);
 
         return Math.Min(maxSheetHeight, r.Request.Height + page.Padding.VerticalThickness);
     }
